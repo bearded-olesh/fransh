78fbcceeaab9d332bcd0bd30d7cdfec7b0461fa1
bearded-olesh <alexeyzharkov88@gmail.com>
2019-06-04 17:48:34 +0300

    Добавил остальные блоки

---
+++ js/form.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#modal-send').on('click enterKey', function(){
+        var modalData = {'name': $('#modal-name').val(),
+                         'phone': $('#modal-phone').val(),
+                         'email': $('#modal-email').val()};
+        $.ajax({
+            url: 'https://.../send.php',
+            type: 'POST',
+            data: modalData,
+            success: function(res){
+            console.log(res);
+            }
+        });
+    });
+
+    $('#footer-send').on('click enterKey', function(){
+        var modalData = {'name': $('#footer-name').val(),
+                         'phone': $('#footer-phone').val(),
+                         'email': $('#footer-email').val()};
+        $.ajax({
+            url: 'https://.../send.php',
+            type: 'POST',
+            data: modalData,
+            success: function(res){
+            console.log(res);
+            }
+        });
+    });
+
+    $('#modal-phone').mask('+7(999) 999-9999');
+    $('#footer-phone').mask('+7(999) 999-9999');
+});
---
+++ js/popup.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    // действия для кнопок
+    $('#know_more').click(function(){
+        show_popup();
+    });
+
+    $('#get_presentation').click(function(){
+        show_popup();
+    });
+
+    $('#popup-close').click(function(){
+        hide_popup();
+    });
+
+    $('#popup_bg').click(function(){
+        hide_popup();
+    });
+});
+ 
+// показать попап
+function show_popup(){
+
+    $('.popup_bg').css({
+        'display': 'block'
+    });
+
+    $('.div_popup').css({
+        'display': 'block'
+    });
+
+    $('body').css({
+        'overflow': 'hidden'
+    });
+    // плавно показываем фон и попап
+    $('.popup_bg').fadeIn(500);       
+    $('.div_popup').fadeIn(500);
+}
+ 
+// скрыть попап
+function hide_popup(){
+    $('body').css({
+        'overflow': 'auto'
+    });
+        console.log("ПОПАЛ!!!")
+    // плавно скрываем попап и фон
+    $('.popup_bg').fadeOut(500);        
+    $('.div_popup').fadeOut(500);
+}
